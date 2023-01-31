d7c4933be3a7de530442d65aca3892e957476437
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2023-01-31 20:04:43 -0300

    Update OpeningScreen - January 31, 2023

--- src/components/ModalBoxes/OpeningScreen/OpeningScreen.jsx
+++ src/components/ModalBoxes/OpeningScreen/OpeningScreen.jsx
@@ -2,32 +2,35 @@ import styles from './OpeningScreen.module.css';
 
 import logoFmei from '../../../assets/imgs/logo-transp-fmei.png';
 
-import doneIcon from '../../../assets/icons/done_outline_icon.svg';
+import upArrowIcon from '../../../assets/icons/up_arrow_icon.svg';
 
 function OpeningScreen(){
-    function closeOpeningScreen(e){
-        e.target.parentElement.style.transform = 'translateY(-100vh)';
+    function closeOpeningScreen(){
+        document.getElementById('openingScreen').style.transform = 'translateY(-100vh)';
     };
 
     setTimeout(() => {
         document.getElementById('startBttn').style.opacity = '1';
-    }, 1700);
+    }, 2000);
 
     return(
-        <div className={styles.openingScreenContainer}>
+        <div className={styles.openingScreenContainer} id='openingScreen'>
             <div className={styles.openingScreenContent}>
                 <figure>
                     <img src={logoFmei} alt="Logo do Facilita MEI" />
                 </figure>
                 <h1>FacilitaMEI</h1>
             </div>
-            <img
-                src={doneIcon} 
-                alt="Ícone de início do aplicativo"
+            <button 
                 className={styles.startButton}
-                onClick={closeOpeningScreen}
                 id='startBttn'
-            />
+                onClick={closeOpeningScreen}
+            >
+                <img
+                    src={upArrowIcon} 
+                    alt="Ícone de seta para cima"
+                />
+            </button>
         </div>
     );
 };
